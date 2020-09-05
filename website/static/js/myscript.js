6b315603a9978e752213e07eda80b6c45b266b9b
Peyton T <peyton.t131@gmail.com>
2020-09-05 12:53:15 -0500

    Add files via upload

---
+++ website/static/js/myscript.js
@@ -0,0 +1,43 @@
+
+function myFunction() {
+      var name = "Joe's bbq and Foot Massage";
+      document.getElementById("myText").innerHTML = name;
+
+      var orderNum = "6942069";
+
+      var totPrice = "420.69";
+      document.getElementById("price").innerHTML = totPrice;
+
+    }
+
+function makeList() {
+    // Establish the array which acts as a data source for the list
+    let listData = [
+        'bbq',
+        'foot massage',
+        'good times',
+    ],
+    // Make a container element for the list
+    listContainer = document.createElement('div'),
+    // Make the list
+    listElement = document.createElement('ol'),
+    // Set up a loop that goes through the items in listItems one at a time
+    numberOfListItems = listData.length,
+    listItem,
+    i;
+
+    // Add it to the page
+    document.getElementsByTagName('ol')[0].appendChild(listContainer);
+    listContainer.appendChild(listElement);
+
+    for (i = 0; i < numberOfListItems; i++) {
+        // create an item for each one
+        listItem = document.createElement('li');
+
+        // Add the item text
+        listItem.innerHTML = listData[i];
+
+        // Add listItem to the listElement
+        listElement.appendChild(listItem);
+    }
+}
