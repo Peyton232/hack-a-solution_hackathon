1505f36d2d68fb889be0b1013de73cfe7d27943f
Peyton T <peyton.t131@gmail.com>
2020-09-05 02:04:08 -0500

    Update myscript.js

--- website/myscript.js
+++ website/myscript.js
@@ -20,3 +20,46 @@ $(document).ready(function(){
 		EasyParallax();
 	});
 });
+
+function myFunction() {
+      var name = "Billy's Shop";
+      document.getElementById("myText").innerHTML = name;
+
+
+    }
+
+function makeList() {
+    // Establish the array which acts as a data source for the list
+    let listData = [
+        'Blue',
+        'Red',
+        'White',
+        'Green',
+        'Black',
+        'Orange',
+        'greenish'
+    ],
+    // Make a container element for the list
+    listContainer = document.createElement('div'),
+    // Make the list
+    listElement = document.createElement('ol'),
+    // Set up a loop that goes through the items in listItems one at a time
+    numberOfListItems = listData.length,
+    listItem,
+    i;
+
+    // Add it to the page
+    document.getElementsByTagName('ol')[0].appendChild(listContainer);
+    listContainer.appendChild(listElement);
+
+    for (i = 0; i < numberOfListItems; i++) {
+        // create an item for each one
+        listItem = document.createElement('li');
+
+        // Add the item text
+        listItem.innerHTML = listData[i];
+
+        // Add listItem to the listElement
+        listElement.appendChild(listItem);
+    }
+}
