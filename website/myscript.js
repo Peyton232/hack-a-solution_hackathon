1e09823bed75480538695b0e0c34cc1aaac9c4d1
Peyton T <peyton.t131@gmail.com>
2020-09-05 10:17:45 -0500

    Update myscript.js

--- website/myscript.js
+++ website/myscript.js
@@ -1,30 +1,4 @@
-//parrallax
-function EasyParallax() {
-	scrollPos = $(this).scrollTop();
-	$('.p1').css({
-		'background-position' : '50% ' + (-scrollPos/4)+"px"
-	});
-  $('.p2').css({
-		'background-position' : '50% ' + (-scrollPos/8)+"px"
-	});
-  $('.p3').css({
-		'background-position' : '70% ' + (-scrollPos/16)+"px"
-	});
-	$('.parallax-text').css({
-		'margin-top': (scrollPos/2)+"px",
-		'opacity': 1-(scrollPos/230)
-	});
-}
-$(document).ready(function(){
-	$(window).scroll(function() {
-		EasyParallax();
-	});
-});
 
-
-
-
-//important
 function myFunction() {
       var name = "Joe's bbq and Foot Massage";
       document.getElementById("myText").innerHTML = name;
